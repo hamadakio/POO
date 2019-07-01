d2f117f532a01d5d975577b7df5616fbd36a3465
Akio Hamada <kiyo.akio@outlook.com>
2019-07-01 05:44:05 -0300

    Class Triangulo add

---
+++ Ex1_UsandoPOO/Ex1_UsandoPOO/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Ex1_UsandoPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+
+            Console.WriteLine("Entre com as medidas do triangulo X :");
+            x.A = double.Parse(Console.ReadLine());
+            x.B = double.Parse(Console.ReadLine());
+            x.C = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nEntre com as medidas do triangulo Y :");
+            y.A = double.Parse(Console.ReadLine());
+            y.B = double.Parse(Console.ReadLine());
+            y.C = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nArea de X = " + x.CalculaArea().ToString("F4"));
+            Console.WriteLine("\nArea de Y = " + y.CalculaArea().ToString("F4"));
+
+            Console.WriteLine(ComparaArea(x.CalculaArea(), y.CalculaArea()));
+
+            Console.ReadKey();
+
+        }
+
+
+        public static string ComparaArea(double v1, double v2)
+        {
+            string output = null;
+
+            if (v1 > v2)
+            {
+                output = "\nMaior Area triangulo X !";
+            }
+            else if (v2 > v1)
+            {
+                output = "\nMaior Area triangulo Y !";
+            }
+            else if (v1 == v2)
+            {
+                output = "\nOs Triangulos tem a mesma AREA !";
+            }
+
+            return output;
+        }
+
+
+    }
+}
---
+++ Ex1_UsandoPOO/Ex1_UsandoPOO/Triangulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ex1_UsandoPOO
+{
+    class Triangulo
+    {
+        //Variaveis
+        public double A;
+        public double B;
+        public double C;
+
+        public double CalculaArea()
+        {
+            double p;
+            double area;
+
+            p = (A + B + C) / 2.0;
+            area = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+
+            return area;
+        }
+
+
+    }
+}
